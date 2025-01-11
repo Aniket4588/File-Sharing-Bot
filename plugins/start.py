6c27676ef23e61cda684c611bff0f691d9d71b4b
Aniket4588 <landeaniket93@gmail.com>
2025-01-11 16:04:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,10 +8,7 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
-madflixofficials = FILE_AUTO_DELETE
 jishudeveloper = madflixofficials
-file_auto_delete = humanize.naturaldelta(jishudeveloper)
-
 
 
 
